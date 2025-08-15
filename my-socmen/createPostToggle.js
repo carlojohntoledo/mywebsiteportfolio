45f8e42fde2b76b9a578e19590d00723fb46bb2f
Carlo John Toledo <toledocarlojohn@gmail.com>
2025-08-16 01:36:25 +0800

    Fixed scripts

---
+++ my-socmen/createPostToggle.js
@@ -0,0 +1,23 @@
+        // create post toggle
+        // Show "Create Project" form
+        document.getElementById('create-new-post').addEventListener('click', function () {
+            const postCard = document.querySelector('.create-card-container-parent');
+            postCard.style.display = 'grid';
+
+            // Reset all input fields
+            const form = postCard.querySelector('#create-project-form');
+            form.reset(); // resets text inputs, textareas, selects
+
+            // Clear file previews
+            const previewContainer = postCard.querySelector('#file-preview-container');
+            previewContainer.innerHTML = "";
+
+            const errorElement = document.querySelector(".error");
+            errorElement.style.display = "none";
+        });
+
+
+        document.getElementById('cancel-btn').addEventListener('click', function () {
+            const postCard = document.querySelector('.create-card-container-parent');
+            postCard.style.display = 'none';
+        });
