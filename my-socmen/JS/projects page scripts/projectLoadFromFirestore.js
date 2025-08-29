89da48b84ed42573d218ee9ec9fa7bb34d114054
Carlo John Toledo <toledocarlojohn@gmail.com>
2025-08-29 12:56:37 +0800

    fix syntax

--- my-socmen/JS/projects page scripts/projectLoadFromFirestore.js
+++ my-socmen/JS/projects page scripts/projectLoadFromFirestore.js
@@ -1,5 +1,15 @@
 
-window.showLoader = window.showLoader || function () { const loader = document.querySelector('.loader-container'); if (loader) loader.style.display = 'flex'; }; window.hideLoader = window.hideLoader || function () { const loader = document.querySelector('.loader-container'); if (loader) loader.style.display = 'none'; };
+window.showLoader = window.showLoader || function () 
+{ 
+    const loader = document.querySelector('.loader-container'); 
+    if (loader) loader.style.display = 'flex';
+}; 
+
+window.hideLoader = window.hideLoader || function () 
+{ 
+    const loader = document.querySelector('.loader-container'); 
+    if (loader) loader.style.display = 'none'; 
+};
 
 
 // =============================================================
